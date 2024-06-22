e6135413494a1cffeb9c6a24993718d7eaaad3fa
Yuki Miyake <miyake_yuuki320@yahoo.co.jp>
2024-06-22 12:37:44 +0900

    rvizファイル選択ボタンを追加

--- launch_turtlebot_buttun.py
+++ launch_turtlebot_buttun.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from tkinter import filedialog
 import subprocess
 import os
 
@@ -11,6 +12,15 @@ def launch_turtlebot3_navigation2():
     command = f"ros2 launch turtlebot3_navigation2 navigation2.launch.py use_sim_time:=True map:={home_dir}/map.yaml"
     subprocess.Popen(['gnome-terminal', '--', 'bash', '-c', command])
 
+def launch_rviz2():
+    rviz_file_path = filedialog.askopenfilename(
+        title="Select RViz Config File",
+        filetypes=[("RViz Config Files", "*.rviz"), ("All Files", "*.*")]
+    )
+    if rviz_file_path:
+        command = f"rviz2 -d {rviz_file_path}"
+        subprocess.Popen(['gnome-terminal', '--', 'bash', '-c', command])
+
 app = tk.Tk()
 app.title("ROS 2 Launcher")
 
@@ -22,5 +32,8 @@ gazebo_button.pack(pady=5)
 
 navigation_button = tk.Button(frame, text="Launch Turtlebot3 Navigation2", command=launch_turtlebot3_navigation2)
 navigation_button.pack(pady=5)
+
+rviz_button = tk.Button(frame, text="Launch RViz2", command=launch_rviz2)
+rviz_button.pack(pady=5)
 
 app.mainloop()
